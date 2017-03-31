02252a845367bf0ff5c53e8103c3b465b8838a4b
joneldiablo <jon_eldiablo@hotmail.com>
2017-03-31 11:17:54 -0600

    generator-plugins
    yeoman generator jquery plugins

---
+++ README.md
@@ -0,0 +1,36 @@
+# generator-plugins [![NPM version][npm-image]][npm-url] [![Build Status][travis-image]][travis-url] [![Dependency Status][daviddm-image]][daviddm-url]
+> generator for custom  jquery plugins
+
+## Installation
+
+First, install [Yeoman](http://yeoman.io) and generator-plugins using [npm](https://www.npmjs.com/) (we assume you have pre-installed [node.js](https://nodejs.org/)).
+
+```bash
+npm install -g yo
+npm install -g generator-plugins
+```
+
+Then generate your new project:
+
+```bash
+yo plugins
+```
+
+## Getting To Know Yeoman
+
+ * Yeoman has a heart of gold.
+ * Yeoman is a person with feelings and opinions, but is very easy to work with.
+ * Yeoman can be too opinionated at times but is easily convinced not to be.
+ * Feel free to [learn more about Yeoman](http://yeoman.io/).
+
+## License
+
+MIT © [JD-R2](elephant-engine.com)
+
+
+[npm-image]: https://badge.fury.io/js/generator-plugins.svg
+[npm-url]: https://npmjs.org/package/generator-plugins
+[travis-image]: https://travis-ci.org/joneldiablo/generator-plugins.svg?branch=master
+[travis-url]: https://travis-ci.org/joneldiablo/generator-plugins
+[daviddm-image]: https://david-dm.org/joneldiablo/generator-plugins.svg?theme=shields.io
+[daviddm-url]: https://david-dm.org/joneldiablo/generator-plugins
---
+++ generators/app/index.js
@@ -0,0 +1,116 @@
+'use strict';
+//copiar archivos de configuracion los que inician con punto, averiguar como colocar el nombre del plugin en los archivos
+//reemplazar todos los textos template por el nombre del nuevo plugin
+var Generator = require('yeoman-generator');
+var chalk = require('chalk');
+var yosay = require('yosay');
+
+function camelCase(string) {
+	return string.replace(/[\.-]([a-z])/ig, function (all, letter) {
+		return letter.toUpperCase();
+	});
+}
+
+module.exports = Generator.extend({
+	prompting: function () {
+
+		var pathName = process.cwd().substr(process.cwd().lastIndexOf("\\") + 1)
+		// Have Yeoman greet the user.
+		this.log(yosay(
+			'jquery plugins template generator!'
+		));
+
+		var prompts = [{
+			type: 'text',
+			name: 'projectName',
+			message: 'Project name',
+			default: pathName
+		}, {
+			type: 'text',
+			name: 'pluginName',
+			message: 'Plugin name, camel case is automatic',
+			default: camelCase(pathName)
+		}];
+
+		return this.prompt(prompts).then(function (props) {
+			// To access props later use this.props.someAnswer;
+			props.pluginName = camelCase(props.pluginName);
+			this.props = props;
+		}.bind(this));
+	},
+
+	writing: function () {
+		this.fs.copyTpl(
+			this.templatePath("demo/index.html"),
+			this.destinationPath("demo/index.html"), {
+				pluginName: this.props.pluginName,
+				projectName: this.props.projectName
+			}
+		);
+		this.fs.copy(
+			this.templatePath("src/DB.json"),
+			this.destinationPath("src/DB.json")
+		);
+		this.fs.copy(
+			this.templatePath("src/DB_routes.json"),
+			this.destinationPath("src/DB_routes.json")
+		);
+		this.fs.copy(
+			this.templatePath("src/template.css"),
+			this.destinationPath("src/" + this.props.projectName + ".css")
+		);
+
+		this.fs.copyTpl(
+			this.templatePath("src/template.js"),
+			this.destinationPath("src/" + this.props.projectName + ".js"), {
+				pluginName: this.props.pluginName
+			}
+		);
+		this.fs.copyTpl( //se usa para reemplazar textos
+			this.templatePath("_bower.json"),
+			this.destinationPath("bower.json"), {
+				projectName: this.props.projectName
+			}
+		);
+
+		this.fs.copy(
+			this.templatePath("_Gruntfile.js"),
+			this.destinationPath("Gruntfile.js")
+		);
+		this.fs.copyTpl(
+			this.templatePath("_karma.conf.js"),
+			this.destinationPath("karma.conf.js"), {
+				projectName: this.props.projectName
+			}
+		);
+		this.fs.copyTpl(
+			this.templatePath("_package.json"),
+			this.destinationPath("package.json"), {
+				projectName: this.props.projectName
+			}
+		);
+		this.fs.copy(
+			this.templatePath("_.editorconfig"),
+			this.destinationPath(".editorconfig")
+		);
+		this.fs.copy(
+			this.templatePath("_.jscsrc"),
+			this.destinationPath(".jscsrc")
+		);
+		this.fs.copy(
+			this.templatePath("_.jshintrc"),
+			this.destinationPath(".jshintrc")
+		);
+		this.fs.copy(
+			this.templatePath("_.npmignore"),
+			this.destinationPath(".npmignore")
+		);
+	},
+
+	install: function () {
+		this.installDependencies();
+	},
+	end: function () {
+		this.log('you can give and get help using $("<selector>").' + this.props.pluginName + '("help");');
+	},
+});
