496460ae6ecaf770b4b489c271f334862d5aa6f8
Jonatha D Rodriguez <jonathan.rodriguez@gonet.us>
2017-05-24 17:40:50 -0500

    Rm dist

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,23 +1,23 @@
 'use strict';
 //copiar archivos de configuracion los que inician con punto, averiguar como colocar el nombre del plugin en los archivos
 //reemplazar todos los textos template por el nombre del nuevo plugin
-var Generator = require( 'yeoman-generator' );
-var chalk = require( 'chalk' );
-var yosay = require( 'yosay' );
+var Generator = require('yeoman-generator');
+var chalk = require('chalk');
+var yosay = require('yosay');
 
-function camelCase( string ) {
-    return string.replace( /[\.-]([a-z])/ig, function( all, letter ) {
-        return letter.toUpperCase( );
-    } );
+function camelCase(string) {
+    return string.replace(/[\.-]([a-z])/ig, function(all, letter) {
+        return letter.toUpperCase();
+    });
 }
-module.exports = Generator.extend( {
-    prompting: function( ) {
-        var pathName = process.cwd( ).substr( process.cwd( ).lastIndexOf( "\\" ) + 1 )
+module.exports = Generator.extend({
+    prompting: function() {
+        var pathName = process.cwd().substr(process.cwd().lastIndexOf("\\") + 1)
             // Have Yeoman greet the user.
-        this.log( yosay(
-            'jquery plugins template generator!'
-        ) );
-        var prompts = [ {
+        this.log(yosay(
+            'jquery plugins template generator v1.1!'
+        ));
+        var prompts = [{
             type: 'text',
             name: 'projectName',
             message: 'Project name',
@@ -26,83 +26,83 @@ module.exports = Generator.extend( {
             type: 'text',
             name: 'pluginName',
             message: 'Plugin name, camel case is automatic',
-            default: camelCase( pathName )
-        } ];
-        return this.prompt( prompts ).then( function( props ) {
+            default: camelCase(pathName)
+        }];
+        return this.prompt(prompts).then(function(props) {
             // To access props later use this.props.someAnswer;
-            props.pluginName = camelCase( props.pluginName );
+            props.pluginName = camelCase(props.pluginName);
             this.props = props;
-        }.bind( this ) );
+        }.bind(this));
     },
-    writing: function( ) {
+    writing: function() {
         this.fs.copyTpl(
-            this.templatePath( "demo/index.html" ),
-            this.destinationPath( "demo/index.html" ), {
+            this.templatePath("demo/index.html"),
+            this.destinationPath("demo/index.html"), {
                 pluginName: this.props.pluginName,
                 projectName: this.props.projectName
             }
         );
         this.fs.copy(
-            this.templatePath( "src/DB.json" ),
-            this.destinationPath( "src/DB.json" )
+            this.templatePath("src/DB.json"),
+            this.destinationPath("src/DB.json")
         );
         this.fs.copy(
-            this.templatePath( "src/DB_routes.json" ),
-            this.destinationPath( "src/DB_routes.json" )
+            this.templatePath("src/DB_routes.json"),
+            this.destinationPath("src/DB_routes.json")
         );
         this.fs.copy(
-            this.templatePath( "src/template.css" ),
-            this.destinationPath( "src/" + this.props.projectName + ".css" )
+            this.templatePath("src/template.css"),
+            this.destinationPath("src/" + this.props.projectName + ".css")
         );
         this.fs.copyTpl(
-            this.templatePath( "src/template.js" ),
-            this.destinationPath( "src/" + this.props.projectName + ".js" ), {
+            this.templatePath("src/template.js"),
+            this.destinationPath("src/" + this.props.projectName + ".js"), {
                 pluginName: this.props.pluginName
             }
         );
         this.fs.copyTpl( //se usa para reemplazar textos
-            this.templatePath( "_bower.json" ),
-            this.destinationPath( "bower.json" ), {
+            this.templatePath("_bower.json"),
+            this.destinationPath("bower.json"), {
                 projectName: this.props.projectName
             }
         );
         this.fs.copy(
-            this.templatePath( "_Gruntfile.js" ),
-            this.destinationPath( "Gruntfile.js" )
+            this.templatePath("_Gruntfile.js"),
+            this.destinationPath("Gruntfile.js")
         );
         this.fs.copyTpl(
-            this.templatePath( "_karma.conf.js" ),
-            this.destinationPath( "karma.conf.js" ), {
+            this.templatePath("_karma.conf.js"),
+            this.destinationPath("karma.conf.js"), {
                 projectName: this.props.projectName
             }
         );
         this.fs.copyTpl(
-            this.templatePath( "_package.json" ),
-            this.destinationPath( "package.json" ), {
+            this.templatePath("_package.json"),
+            this.destinationPath("package.json"), {
                 projectName: this.props.projectName
             }
         );
         this.fs.copy(
-            this.templatePath( "_.editorconfig" ),
-            this.destinationPath( ".editorconfig" )
+            this.templatePath("_.editorconfig"),
+            this.destinationPath(".editorconfig")
         );
         this.fs.copy(
-            this.templatePath( "_.jscsrc" ),
-            this.destinationPath( ".jscsrc" )
+            this.templatePath("_.jscsrc"),
+            this.destinationPath(".jscsrc")
         );
         this.fs.copy(
-            this.templatePath( "_.jshintrc" ),
-            this.destinationPath( ".jshintrc" )
+            this.templatePath("_.jshintrc"),
+            this.destinationPath(".jshintrc")
         );
         this.fs.copy(
-            this.templatePath( "_.npmignore" ),
-            this.destinationPath( ".npmignore" )
+            this.templatePath("_.npmignore"),
+            this.destinationPath(".npmignore")
         );
     },
-    install: function( ) {
-        this.installDependencies( );
+    install: function() {
+        this.installDependencies();
     },
-    end: function( ) {
-        this.log( 'you can give and get help using $.' + this.props.pluginName + '("help");' );
+    end: function() {
+        this.log('you can give and get help using $.' + this.props.pluginName + '("help");');
     },
-} );
+});
